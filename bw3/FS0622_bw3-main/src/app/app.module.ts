895a9953ae646d771ecc029460785b9c630e7ddd
Andre24 <cursi.andre@gmail.com>
2023-02-06 18:42:13 +0100

    first commit

---
+++ bw3/FS0622_bw3-main/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { PostComponent } from './components/post/post.component';
+import { CardComponent } from './components/card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PostComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
